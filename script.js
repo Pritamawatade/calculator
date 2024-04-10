4f273b9df395ab3d09eed7b8d6fba9543929277e
pritamawatade <pritamawatade5@gmail.com>
2024-04-10 11:50:57 +0530

    added some js logic

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
+// let string = "";
+let buttons = document.querySelectorAll(".button");
+console.log(".... for loop");
 
 Array.from(buttons).forEach((button) => {
-    button.addEventlistner('click',()=>{
-        console.log(e.target);
-    })
+  console.log("inside for loop");
 
+  button.addEventListener("click", (e) => {
+    console.log(e.target);
+    console.log("aise of for loop");
+  });
 });
+
+console.log("outaise of for loop");
